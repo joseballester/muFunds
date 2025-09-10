154650fd28ab9269998ad564c95a713cda8308be
Jose Ballester <jose@ballester.me>
2025-09-10 12:20:08 +0200

    Add unit test for Spanish pension plan in quefondos

--- test/quefondos_unit.js
+++ test/quefondos_unit.js
@@ -3,7 +3,7 @@ const newTestContext = require('./mocks');
 
 const UrlFetchAppFromFile = require('./mocks/UrlFetchApp_file');
 
-describe('[quefondos] Unit tests', () => {
+describe('[quefondos] Unit tests (mutual fund: IE00B03HD191)', () => {
   const id = 'IE00B03HD191';
   const source = 'quefondos';
 
@@ -34,6 +34,54 @@ describe('[quefondos] Unit tests', () => {
   it('should throw an error for expenses', () => {
     try {
       testContext.muFunds('expenses', id, source);
+      assert.fail('Expected error was not thrown');
+    } catch (error) {
+      assert.equal(error.message, 'Expenses ratio is not available from this source');
+    }
+  });
+
+  it('should return category', () => {
+    const category = testContext.muFunds('category', id, source);
+    assert.equal(category, 'RVI GLOBAL');
+  });
+});
+
+describe('[quefondos] Unit tests (Spanish pension plan: N5396)', () => {
+  const id = 'N5396';
+  const source = 'quefondos';
+
+  const testContext = newTestContext({
+    UrlFetchApp: UrlFetchAppFromFile,
+  });
+
+  it('should return NAV', () => {
+    const nav = testContext.muFunds('nav', id, source);
+    assert.equal(nav, 16.23623);
+  });
+
+  it('should return date', () => {
+    const date = testContext.muFunds('date', id, source);
+    assert.equal(date, '05/09/2025');
+  });
+
+  it('should return change', () => {
+    try {
+      testContext.muFunds('change', id, source);
+      assert.fail('Expected error was not thrown');
+    } catch (error) {
+      assert.equal(error.message, 'Last change is not available for this asset and source. Please try another data source.');
+    }
+  });
+
+  it('should return currency', () => {
+    const currency = testContext.muFunds('currency', id, source);
+    assert.equal(currency, 'EUR');
+  });
+
+  it('should throw an error for expenses', () => {
+    try {
+      testContext.muFunds('expenses', id, source);
+      assert.fail('Expected error was not thrown');
     } catch (error) {
       assert.equal(error.message, 'Expenses ratio is not available from this source');
     }
